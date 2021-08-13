1a8ad2d602abea7ab6c2c9e5d52562a5f79c4915
Alfredo Colito <acolito@cpqd.com.br>
2021-08-12 22:53:39 -0300

    Resolve desafio

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -31,14 +31,30 @@ export function Home() {
   async function loadData() {
     const dataKey = '@savepass:logins';
     // Get asyncStorage data, use setSearchListData and setData
+    const loginData = await AsyncStorage.getItem(dataKey);
+    if(!loginData) return;
+    
+    setData(JSON.parse(loginData));
+    setSearchListData(JSON.parse(loginData));
   }
 
   function handleFilterLoginData() {
     // Filter results inside data, save with setSearchListData
+    if(!searchText){
+      loadData();
+    }
+    else{
+      setSearchListData(oldState => 
+        oldState.filter(
+          login => login.service_name.toLowerCase().includes(searchText.toLowerCase())
+          )
+        );      
+    }
   }
 
   function handleChangeInputText(text: string) {
     // Update searchText value
+    setSearchText(text);
   }
 
   useFocusEffect(useCallback(() => {
--- src/screens/RegisterLoginData/index.tsx
+++ src/screens/RegisterLoginData/index.tsx
@@ -50,6 +50,19 @@ export function RegisterLoginData() {
     const dataKey = '@savepass:logins';
 
     // Save data on AsyncStorage and navigate to 'Home' screen
+    const loginData = await AsyncStorage.getItem(dataKey);
+    const currentLoginData = loginData ? JSON.parse(loginData) : [];
+
+    const loginDataFormatted = [
+      ...currentLoginData,
+      newLoginData
+    ];
+
+    // Save data on AsyncStorage
+    await AsyncStorage.setItem(dataKey, JSON.stringify(loginDataFormatted));
+
+    navigate("Home");
+    
   }
 
   return (
@@ -66,8 +79,7 @@ export function RegisterLoginData() {
             title="Nome do serviço"
             name="service_name"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.service_name && errors.service_name.message
             }
             control={control}
             autoCapitalize="sentences"
@@ -78,8 +90,7 @@ export function RegisterLoginData() {
             title="E-mail"
             name="email"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.email && errors.email.message
             }
             control={control}
             autoCorrect={false}
@@ -91,8 +102,7 @@ export function RegisterLoginData() {
             title="Senha"
             name="password"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.password && errors.password.message
             }
             control={control}
             secureTextEntry
